11b2f32281e8fd763737ded4213ed21654511585
Dom McKellar <dom.mckellar@gmail.com>
2016-02-01 12:43:38 -0800

    Add page titles

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -6,17 +6,20 @@ class PagesControllerTest <  ActionController::TestCase
   test "should get home" do
     get :show, id: 'home'
     assert_response :success
+    assert_select "title", "Home | BottomLine Delivery"
   end
 
   test "should get about" do
     #get :about
     get :show, id: 'about'
     assert_response :success
+    assert_select "title", "About | BottomLine Delivery"
   end
 
   test "should get contact" do
     get :show, id: 'contact'
     assert_response :success
+    assert_select "title", "Contact | BottomLine Delivery"
   end
 
 end
