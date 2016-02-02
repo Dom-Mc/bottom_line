5c9bfccce35e3e19bf99cfcd83ce800e57e07c14
Dom McKellar <dom.mckellar@gmail.com>
2016-02-02 09:26:17 -0800

    Add full_title helper

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,24 +2,27 @@ require 'test_helper'
 
 class PagesControllerTest <  ActionController::TestCase
   tests HighVoltage::PagesController
+  def setup
+    @base_title = "BottomLine Delivery"
+  end
 
   test "should get home" do
     get :show, id: 'home'
     assert_response :success
-    assert_select "title", "Home | BottomLine Delivery"
+    assert_select "title", "Welcome | #{@base_title}"
   end
 
   test "should get about" do
     #get :about
     get :show, id: 'about'
     assert_response :success
-    assert_select "title", "About | BottomLine Delivery"
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get :show, id: 'contact'
     assert_response :success
-    assert_select "title", "Contact | BottomLine Delivery"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
